c62d7c26bb750828e932561de1eed39b2bbbd99f
Yajaira Calvache <tienspopayan9@gmail.com>
2024-07-08 21:22:56 -0500

    primer commit

---
+++ 03.input.py
@@ -0,0 +1,8 @@
+#print("Digite su nombre: ")
+#input("Digite su nombre: ")
+#input("Digite su apellido: ")
+
+#print(input("Digite su apellido: ")
+
+print("Tu nombre es:" + input("Digite su nombre"))
+      
